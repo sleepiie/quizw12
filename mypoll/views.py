77463bdf7e5ad7f91073d242b3f908362a1d0022
sleepiie <122366817+sleepiie@users.noreply.github.com>
2025-02-13 16:00:33 +0700

    end quizw12

--- mypoll/views.py
+++ mypoll/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from mypoll.models import Question , Choice
+from django.utils import timezone
 
-def home_page(request):
-    return render(request, "home.html")
+def home_page(request): 
+
+    question = Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+
+    return render(request, "home.html" , {'question':question})
