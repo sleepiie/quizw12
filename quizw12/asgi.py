c29649f6a991c7f4178f204070f3593a7193ec93
sleepiie <122366817+sleepiie@users.noreply.github.com>
2025-02-13 14:51:40 +0700

    create project quizw12

---
+++ quizw12/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for quizw12 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quizw12.settings')
+
+application = get_asgi_application()
