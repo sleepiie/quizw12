929195ba917f820f6ef44baf1c19a147084afe17
sleepiie <122366817+sleepiie@users.noreply.github.com>
2025-02-15 19:18:07 +0700

    update show choice

--- mypoll/views.py
+++ mypoll/views.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 
 def home_page(request): 
 
-    question = Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+    latest_question_list = Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+    
 
-    return render(request, "home.html" , {'question':question})
+    return render(request, "home.html" , {'latest_question_list':latest_question_list})
