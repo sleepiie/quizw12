5f31a2a027244c8c6038f5d7adb8c5833f25757a
sleepiie <122366817+sleepiie@users.noreply.github.com>
2025-03-10 15:11:15 +0700

    add rating function
    เพิ่มการจัดเรทติ้งของคำถามโดยนับจากจำนวน โหวตของคำถามนั้นๆ

--- mypoll/views.py
+++ mypoll/views.py
@@ -3,11 +3,29 @@ from mypoll.models import Question , Choice
 from django.utils import timezone
 
 def home_page(request): 
-
     latest_question_list = Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
-    
 
-    return render(request, "home.html" , {'latest_question_list':latest_question_list})
+    choice_vote = 0
+    ratings = []
+
+    for i in range (1,len(latest_question_list)):
+        question = Question.objects.get(id=i)
+        for j in range (1,5):
+            choice = question.choice_set.get(pk=j)
+            choicevote=choice.votes
+            choice_vote += choicevote
+            if choice_vote >= 50:
+                rating = "hot"
+            elif choice_vote>=10 and choice_vote<=50:
+                rating = "warm"
+            else:
+                rating="normal"
+            ratings.append(rating)
+            
+         
+    print(ratings)
+
+    return render(request, "home.html" , {'latest_question_list':latest_question_list , 'rating':rating})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -26,3 +44,5 @@ def vote(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'results.html', {'question': question})
+
+    
