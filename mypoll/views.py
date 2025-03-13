23fb2388e965ad6d9319e61becd431ca49514e48
sleepiie <122366817+sleepiie@users.noreply.github.com>
2025-03-13 14:24:41 +0700

    add private poll page and private poll list function

--- mypoll/views.py
+++ mypoll/views.py
@@ -3,7 +3,7 @@ from mypoll.models import Question , Choice
 from django.utils import timezone
 
 def home_page(request): 
-    latest_question_list = Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+    latest_question_list = Question.objects.filter(pub_date__lte=timezone.now(),is_private=False).order_by("-pub_date")[:5]
 
     question_rating = []
 
@@ -43,4 +43,26 @@ def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'results.html', {'question': question})
 
-    
+
+def private_poll(request):
+    latest_question_list = Question.objects.filter(pub_date__lte=timezone.now(),is_private=True).order_by("-pub_date")[:5]
+
+    question_rating = []
+
+    for question in latest_question_list:
+        total_votes = 0
+        choices = question.choice_set.all()
+        for choice in choices:
+            total_votes += choice.votes
+
+        if total_votes >= 50:
+            rating = "!!Hot"
+        elif total_votes >= 10:
+            rating = "!Warm"
+        else:
+            rating = ""
+                
+        question.rating = rating
+        question_rating.append(question)
+
+    return render(request, "private_poll.html" , {'latest_question_list':question_rating})
